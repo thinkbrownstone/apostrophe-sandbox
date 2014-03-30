6fd97a7b097fb8b60412233f0cc5e3decddadf81
Brian Feister <brian@drm.st>
2014-03-30 16:07:09 -0400

    Add simple gruntfile for generating spritemap as SVG with PNG fallback for older browsers

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+
+  grunt.initConfig({
+
+      //MINIFICATION TASKS
+      // The following *-min tasks produce minified files in the dist folder
+      // imagemin: {
+      //   dist: {
+      //     files: [
+      //       {
+      //         expand: true,
+      //         cwd: 'public/images',
+      //         src: '{,*/}*.{png,jpg,jpeg,gif}',
+      //         dest: 'public/images'
+      //       }
+      //     ]
+      //   }
+      // },
+      'svg-sprites': {
+        options: {
+          spriteElementPath: 'public/svg/sprites',
+          spritePath: 'public/img',
+          cssPath: 'public/css'
+        },
+        shapes: {
+          options: {
+            sizes: {
+              large: 75,
+              medium: 30,
+              small: 15
+            },
+            refSize: 26,
+            unit: 13
+          }
+        }
+      },
+
+    }
+  );
+
+  grunt.registerTask('default', [
+    // 'imagemin',
+    'svg-sprites'
+  ]);
+};
