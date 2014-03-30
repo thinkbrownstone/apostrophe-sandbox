c252fad5f1094568790644de04a5078822403d4a
Brian Feister <brian@drm.st>
2014-03-30 13:48:14 -0400

    Add Kevin's (?) JS for handling site navigation toggling

--- public/js/site.js
+++ public/js/site.js
@@ -36,5 +36,98 @@
 // });
 
 $(function() {
+
+    /* Site-wide JS goes here
+    ------------------------------------------------------------ */
+
+    function setToWindowHeight(el) {
+        if(!$(el).length) {
+            return;
+        }
+
+        var el = $(el);
+        var windowHeight = $(window).height();
+        el.height(windowHeight);
+    }
+
+    /*
+        applies a class to an element at a specified window scroll position, throttled to check every 50ms
+        onStart and onEnd events to apply different styling
+    */
+    var StickyElement = function (element, data) {
+        this.element = element;
+        this.className = data.className;
+        this.startPosition = data.startPosition || 0;
+        this.endPosition = data.endPosition || null;
+        this.onStart = data.onStart || function () { };
+        this.onEnd = data.onEnd || function () { };
+        this.throttleDuration = data.throttle || 50;
+        this.isThrottled = false;
+
+        if (this.element.length !== 0) {
+            this.init();
+        }
+    };
+
+    StickyElement.prototype.init = function () {
+        var self = this;
+
+        $(window).bind("scroll", function () {
+            if (self.isThrottled) {
+                return;
+            }
+            self.isThrottled = true;
+            setTimeout(function () {
+                self.checkScroll();
+                self.isThrottled = false;
+            }, self.throttleDuration);
+        });
+    };
+
+    StickyElement.prototype.checkScroll = function () {
+        var winScroll = $(window).scrollTop();
+        if (winScroll >= this.startPosition) {
+            if (this.endPosition !== null) {
+                if (winScroll <= this.endPosition) {
+                    if (!this.element.hasClass(this.className)) {
+                        this.element.addClass(this.className);
+                        this.onStart.call();
+                    }
+                } else {
+                    if (this.element.hasClass(this.className)) {
+                        this.element.removeClass(this.className);
+                        this.onEnd.call();
+                    }
+                }
+            } else {
+                if (!this.element.hasClass(this.className)) {
+                    this.element.addClass(this.className);
+                    this.onStart.call();
+                }
+            }
+        } else {
+            if (this.element.hasClass(this.className)) {
+                this.element.removeClass(this.className);
+                this.onEnd.call();
+            }
+        }
+    };
+
+    (function($){
+        setToWindowHeight('.content-header-home');
+        if ($('.site-header').length) {
+            var nav = new StickyElement($('.site-header'), {
+                className : 'sticky',
+                startPosition : $('.site-header').offset().top + $('.site-header').height(),
+                onStart : function() {
+                    $('.content-header').addClass('content-header-sticky');
+                },
+                onEnd : function() {
+                    $('.content-header').removeClass('content-header-sticky');
+                }
+            });
+        }
+    })(jQuery);
+
 });
 
